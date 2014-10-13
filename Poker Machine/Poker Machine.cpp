f693c010f6fc3765fa9caab2974382a7d7e024f7
ROTNdev <rotn.edm@gmail.com>
2014-10-13 18:03:10 +1100

    :slot_machine: More Reels testing

--- Poker Machine/Poker Machine.cpp
+++ Poker Machine/Poker Machine.cpp
@@ -4,28 +4,24 @@
 #include "stdafx.h"
 #include "PokerCard.h"
 #include "Reel.h"
+#include <Windows.h>
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[]) {
     srand(time(0));
-    cout << "Hello, world" << endl;
+    system("color a");
     Reel reel = Reel();
-    list<PokerCard> reelCards = reel.getCards();
-    for (list<PokerCard>::iterator it = reelCards.begin(); it != reelCards.end(); ++it) {
-        cout << it->getId() << "|" << it->getSuit() << endl;
-    }
-    cout << endl << "====================" << endl;
-    reel.iterateOnce();
-    reelCards = reel.getCards();
-    for (list<PokerCard>::iterator it = reelCards.begin(); it != reelCards.end(); ++it) {
-        cout << it->getId() << "|" << it->getSuit() << endl;
-    }
-    cout << endl << "====================" << endl;
-    reel.iterateOnce();
-    reelCards = reel.getCards();
-    for (list<PokerCard>::iterator it = reelCards.begin(); it != reelCards.end(); ++it) {
-        cout << it->getId() << "|" << it->getSuit() << endl;
+    list<PokerCard> reelCards;
+    while (true) {
+        COORD position = { 0, 0 };
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+        reel.iterateOnce();
+        reelCards = reel.getCards();
+        for (list<PokerCard>::iterator it = reelCards.begin(); it != reelCards.end(); ++it) {
+            cout << it->getId() << "|" << it->getSuit() << endl;
+        }
+        Sleep(100);
     }
     system("pause");
 	return 0;
